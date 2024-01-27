7b00b5375a5364d71ea0289b74514c3d70348461
funmerc <jasar2100@gmail.com>
2024-01-27 15:46:59 -0700

    server - updated configuration for vue home page and added/linked default vue app

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.use('/topics', express.static(path.join(__dirname, '../dist/angular-topics-app')))
-// app.use('/vue', express.static(path.join(__dirname, '../dist/vue-landing-app'))) //TODO - add app
+app.use('/home', express.static(path.join(__dirname, '../dist/vue-landing-app')))
 // app.use('/react', express.static(path.join(__dirname, '../dist/react-references-app'))) //TODO - add app
 
 
@@ -18,9 +18,11 @@ app.get('/topics', (_req, res) => {
     res.status(200).sendFile(path.join(__dirname, '../dist/angular-topics-app/index.html'))
 })
 
-app.get('*', (_req, res) => {
-    res.status(200).send("You hit the server!")
+// Vue Page
+app.get('/home', (_req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../dist/vue-landing-app/index.html'))
 })
+app.get('*', (_req, res) => res.redirect('/home'))
 
 app.listen(port, () => {
     logger.log(`Server running on ${port}.`)
---
+++ vue-landing-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="main">VUE LANDING PAGE PLACEHOLDER</main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.main {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
