b3ef09a20076018df0a4020f8ef5557179e61279
funmerc <jasar2100@gmail.com>
2024-01-27 14:58:31 -0700

    server - updated configurations and added/linked blank angular project to topics

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,22 @@
 import express from 'express'
+import path from 'path'
 import Logger from './modules/logger/logger'
 
 const logger = Logger('index.ts')
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+app.use('/topics', express.static(path.join(__dirname, '../dist/angular-topics-app')))
+// app.use('/vue', express.static(path.join(__dirname, '../dist/vue-landing-app'))) //TODO - add app
+// app.use('/react', express.static(path.join(__dirname, '../dist/react-references-app'))) //TODO - add app
+
+
+// Angular Page
+app.get('/topics', (_req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../dist/angular-topics-app/index.html'))
+})
 
 app.get('*', (_req, res) => {
     res.status(200).send("You hit the server!")
