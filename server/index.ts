8efec66effcd8269cf96a02768afe35600187f7d
funmerc <jasar2100@gmail.com>
2024-03-02 17:14:52 -0700

    develop: fixing copy structure command for deployment

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ if (process.env?.LOCAL) {
   )
 }
 
-const folderLocation = `${process.env?.LOCAL ? '../dist/' : './dist/'}`
+const folderLocation = `${process.env?.LOCAL ? '../dist/' : './'}`
 
 app.use(
   '/topics',
